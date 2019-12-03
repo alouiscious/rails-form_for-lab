10db770d13ca84d989448df42ac3e13db4d45243
Alouiscious Media <info@reneesoul.com>
2019-12-03 01:56:23 -0800

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -15,12 +15,13 @@ class SchoolClassesController < ApplicationController
   def create
     @school_class = SchoolClass.new(school_class_params(:title, :room_number))
     @school_class.save
-    redirect_to @school_class
+    redirect_to school_class_path(@school_class)
   end
 
   def update
     @school_class = SchoolClass.find(params[:id])
     @school_class = SchoolClass.update(:title, :room_number)
+    redirect_to school_class_path(@school_class)
   end
 
   def edit
